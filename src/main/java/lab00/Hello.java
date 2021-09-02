9ebe1359c254da99da1740e872e91e86b4fa2911
Katy Martinson <kam071@bucknell.edu>
2021-09-02 14:14:01 -0400

    Lab00, Exercise 5, Complete

---
+++ src/main/java/lab00/Hello.java
@@ -0,0 +1,23 @@
+/* *****************************************
+* CSCI205 - Software Engineering and Design
+* Fall 2021
+* Instructor: Professor King
+* Section: 02 - 9:50am
+*
+* Name: Katy Martinson
+* Date: 9/2/2021
+*
+* Lab / Assignment: Lab00
+*
+* Description:
+* Learning to create a java environment
+* *****************************************/
+
+package lab00;
+public class Hello
+{
+ public static void main( String[] args ){
+ System.out.println("Programming is not a spectator sport!");
+ System.exit(0);
+ }
+}
